d40919cdcf394cc223184c227b7a2aded55b3394
Joselon <jnavarro@oesia.com>
2015-08-03 23:30:20 +0200

    Borrar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -82,3 +82,11 @@ exports.update=function(req,res){
 	});
 
 };
+
+//DELETE 
+
+exports.destroy=function(req,res){
+	req.quiz.destroy().then(function(){
+	 res.redirect('/quizes');})
+	.catch(function(error){next(error)});
+};
