83ba77392ec1ebefcc7bbb08af2f62aa6e98b023
Joselon <jnavarro@oesia.com>
2015-08-03 23:06:33 +0200

    Editar preguntas(cambia metodo findById en load

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,7 +2,7 @@ var models=require('../models/models.js');
 
 // Autoload -factoriza el codigo si ruta incluye :quizId
 exports.load=function(req, res, next, quizId){
-	models.Quiz.find(quizId).then(
+	models.Quiz.findById(quizId).then(
 	 function(quiz){
 	  if(quiz){
 		req.quiz=quiz;
@@ -58,4 +58,27 @@ exports.create = function(req, res) {
 			})
 		}
 	});		
+};
+
+// GET /quizes/:id/edit
+exports.edit=function(req,res){
+	var quiz=req.quiz; //autoload de instancia de quiz
+	res.render('quizes/edit', {quiz:quiz, errors:[]});
+};
+
+//PUT /quizes/:id
+exports.update=function(req,res){
+	req.quiz.pregunta= req.body.quiz.pregunta;
+	req.quiz.respuesta=req.body.quiz.respuesta;
+
+	req.quiz.validate().then(function(err){
+		if(err){
+		 res.render('quizes/edit',{quiz:req.quiz,errors: err.errors});
+		} else{
+		 req.quiz // save: guarda campos pregunta y respuesta en DB
+		 .save( {fields: ["pregunta","respuesta"]})
+		 .then( function(){res.redirect('/quizes');});
+		} //Redireccion HTTP a lista de preguntas (URL relativo)
+	});
+
 };
