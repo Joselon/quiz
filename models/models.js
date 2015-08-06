b1ff6bf4227479f7059f2b5e18c52aadab5e2633
Joselon <jnavarro@oesia.com>
2015-08-06 22:09:30 +0200

    Crear comentario

--- models/models.js
+++ models/models.js
@@ -31,8 +31,15 @@ var sequelize= new Sequelize(DB_name,user,pwd,
 var quiz_path=path.join(__dirname,'quiz');
 var Quiz= sequelize.import(quiz_path);
 
+//Imiportar definicion de la tabla Commnet
+var comment_path=path.join(__dirname,'comment');
+var Comment=sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
 
 exports.Quiz=Quiz; // exportar definicion de tabla Quiz
+exports.Comment=Comment; //exportamos definicion de tabla Comment
 
 // sequelize.sync() crea e inicializa tabla de preguntas de DB
 sequelize.sync().then(function() {
--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var commentController=require('../controllers/comment_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  console.log("ESTOY EN INDEX");
+ // console.log("ESTOY EN INDEX");
   res.render('index', { title: 'Quiz',errors:[] });
 });
 
+//GET pagina de autor
 router.get('/author', function(req, res) {
   res.render('author',{errors:[]});	
 });
@@ -25,5 +27,8 @@ router.post('/quizes/create',			quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit',	quizController.edit);
 router.put('/quizes/:quizId(\\d+)',		quizController.update);
 router.delete('/quizes/:quizId(\\d+)',		quizController.destroy);
+
+router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',	 commentController.create);
 
 module.exports = router;
