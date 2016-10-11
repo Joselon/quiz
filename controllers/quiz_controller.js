4f43c30011e1af34b554f04e68e0f12621afdbd9
Joselon <jose.navarro.osta@gmail.com>
2016-10-11 14:23:12 +0200

    Corregida definicion url busqueda

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -19,7 +19,7 @@ exports.load=function(req, res, next, quizId){
 exports.index=function(req, res){
 var urlBusqueda= '/quizes';
  if (req.query.search !== undefined){
-  urlBusqueda= window.location.href;
+  urlBusqueda= javascript:window.location.href;
    //si estamos buscando filtramos las preguntas
    //sustituimos en blanco por %
   if(req.query.search.charAt(0)!='~'){
