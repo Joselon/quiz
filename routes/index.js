9a5f892e0e101de7975dbb6b2d562bc8d3e82776
Joselon79 <Joselon79@192.168.1.130>
2016-10-12 11:51:39 +0200

    Solo ocultar2

--- routes/index.js
+++ routes/index.js
@@ -41,7 +41,7 @@ router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',	 commentController.create);
 router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
 					 sessionController.loginRequired, commentController.publish);
-router.put('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/hide',
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/hide',
 					 sessionController.loginRequired, commentController.hide);                                                                              
 /*router.delete('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/destroy',	 sessionController.loginRequired,commentController.destroy);
 // Definicion de rutas de Estadisticas
