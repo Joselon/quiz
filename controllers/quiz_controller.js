51481f7ad5ca27515d31174c73483dc12b6596a7
Joselon79 <Joselon79@192.168.1.130>
2016-10-15 17:19:52 +0200

    OrdenaBusquedaComentarios6

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -31,8 +31,8 @@ exports.index=function(req, res){
 		  searchAux[i]=Number(searchAux[i]);
 	  }
 	  */
-	   models.Quiz.findAll({ include: [{model:models.Comment,
-                                 where:['"Comments.createdAt" is not null']}],
+	   models.Quiz.findAll({ include: [{model:models.Comment}],
+                        where:['"Comments.createdAt" is not null'],
 			order:[["Comments.createdAt","DESC"]]}).then(function(quizes){
 		res.render('quizes/index',{quizes:quizes, errors:[],urlBusqueda:urlBusqueda});
 		});
